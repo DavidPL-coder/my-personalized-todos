bbf54bbc86f44d9a9dae51603eaac18c92a57f94
DavidPL-coder <dawid2002@hotmail.com>
2023-08-09 17:58:37 +0200

    Edit pull-create-start.sh.

--- pull-create-start.sh
+++ pull-create-start.sh
@@ -1,4 +1,4 @@
 git restore .
 git pull
-sudo chmod u+x create-start.sh hard-create-start.sh start.sh
+sudo chmod u+x pull-create-start.sh create-start.sh hard-create-start.sh start.sh
 ./create-start.sh
